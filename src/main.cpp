3090450201ee315832018d97fcd8cdd5673a9403
awesomearjun <iamawesomearjun@gmail.com>
2025-03-02 15:31:30 -0500

    Render Window

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,65 @@
+#include "SDL3/SDL_render.h"
+#include <SDL3/SDL_error.h>
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
-int main(int argc, char *argv[]) {
-    SDL_Log("%s", "HELLO");
+int main(int argc, char *argv[])
+{
+    SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;    
+
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS))
+    {
+        SDL_Log("Error initializing: %s", SDL_GetError());
+        return 1;
+    }
+
+    window = SDL_CreateWindow("SDL3 Ping Pong", 800, 600, 0);
+
+    if (window == NULL)
+    {
+        SDL_Log("SDL_CreateWindow Error: %s", SDL_GetError());
+        return 2;
+    }
+
+    renderer = SDL_CreateRenderer(window, NULL);
+    if (!SDL_SetRenderVSync(renderer, 1))
+    {
+        SDL_Log("SDL Renderer Vsync error: ", SDL_GetError());
+    }
+    
+    
+    if (renderer == NULL)
+    {
+        SDL_Log("Create Renderer Error: %s", SDL_GetError());
+        return 3;
+    }
+
+    SDL_Event event;
+    bool gameOver = false;
+
+    while (!gameOver)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+            case SDL_EVENT_QUIT:
+                SDL_Log("SDL3 event quit");
+                gameOver = true;
+                break;
+            
+            default:
+                break;
+            }
+        }
+        
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+
+    return 0;
 }
