5b2833555111b35935453fd798d1a1f765456bfa
awesomearjun <iamawesomearjun@gmail.com>
2025-03-15 15:20:44 -0400

    Add install script

---
+++ scripts/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+mkdir vendor
+cd vendor
+echo "set(SDL_TEST OFF)
+
+add_subdirectory(SDL EXCLUDE_FROM_ALL)
+add_subdirectory(SDL_image EXCLUDE_FROM_ALL)
+
+add_library(vendor INTERFACE)
+target_link_libraries(vendor INTERFACE SDL3::SDL3 SDL3_image::SDL3_image)" > CMakeLists.txt
+git submodule add https://github.com/libsdl-org/SDL.git SDL
+cd SDL
+git checkout 3b9f0dff1023dfde49e645e318cf7cfb7b83c7ba
+cd ../
+git submodule add https://github.com/libsdl-org/SDL_image.git SDL_image
+cd SDL_image
+git checkout d4153324124963b8003e4e5b1d6743661969fdf5
+cd ../../
+echo "All done 👍"
